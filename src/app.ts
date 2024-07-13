0cdf682c0b7904f6a9a830091f7c444d251d7c39
kazirauf <raufelahi9876@gmail.com>
2024-07-13 23:08:37 +0600

    alleror is done

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://leafy-cart-389.web.app'],
+    origin: ['http://localhost:5173', 'https://pure-planet-fdda8.web.app'],
   }),
 );
 
